6a8ef10ab3a89c5cc6d904155bfa465c69c30de4
Brock Ellis <brock@sharproot.com>
2014-09-23 23:09:11 -0500

    Revamp config file and pretty print json

--- sycamore.js
+++ sycamore.js
@@ -1,12 +1,27 @@
 #! /usr/bin/env node
-var config = require('./config.json')
+var fs = require('fs');
 var program = require('commander');
 var request = require('request');
+var userhome = require('userhome');
+var prettyjson = require('prettyjson');
+
+var configFile = userhome('.sycamorecli.json');
+if(!fs.existsSync(configFile)){
+    //config doesn't exist yet and needs to be created
+    console.log("No config file present.");
+    console.log("Creating file at " + configFile);
+    console.log("Please edit the config file and add your unique settings.");
+    fs.writeFileSync(configFile, fs.readFileSync(__dirname+"/.sycamorecli.json"));
+}
+
+var config = JSON.parse( fs.readFileSync(userhome(configFile), 'utf8')  );
 
 program
     .version('0.0.1')
-    .usage('<keywords>')
-    .option('-u, --user [alias]', 'The user object set up in the config file to use')
+    .usage('<endpoint>')
+    .option('-u, --user [string]', 'Which user to send the request as')
+    .option('-t, --token [string]', 'Specific token to use for this request')
+    .option('-v, --verbose', 'Display additional details about request')
     .parse(process.argv);
 
 if(!program.args.length){
@@ -14,13 +29,30 @@ if(!program.args.length){
 }else{
     var endpoint = program.args;
 
-    if(!program.user){
-        var token = config.users.default.token;
+    if(!program.token){
+        if(!program.user){
+            try{
+                var token = config.users.default.token;
+            }catch(e){
+                console.log("No default user defined!");
+                program.help();
+            }
+        }else{
+            try{
+                var token = config.users[program.user].token;
+            }catch(e){
+                console.log("Requested user is not defined!");
+                program.help();
+            }
+        }
     }else{
-        var token = config.users[program.user].token;
+        var token = program.token;
     }
-
-    console.log("Fetching endpoint " + endpoint + " with token " + token);
+    
+    if(program.verbose){
+        console.log("Fetching endpoint " + endpoint);
+        console.log("Using token " + token);
+    }
 
     request({
         
@@ -28,18 +60,18 @@ if(!program.args.length){
         headers: {
             "Authorization": "Bearer " + token
         },
-        url: "https://app.sycamoreeducation.com/api/v1/"+endpoint
+        url: "http://dev.sycamoreeducation.com/api/v1/"+endpoint
     
     }, function(error, response, body){
     
-        if(!error && response.statusCode == 200){
-            console.log(body);
-        }else if(error){
+        if(!error){
+            if(program.verbose)
+                console.log("Response code: " + response.statusCode);
+        
+            console.log( prettyjson.render( JSON.parse(body) ) );
+        }else{
             console.log("Error: " + error);
         }
 
     });
-
-    console.log("Keywords: " + program.args);
 }
-
